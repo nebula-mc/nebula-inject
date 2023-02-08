cab5e1d0d56a8026809b1bb02573792a65b50968
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-02-08 18:25:51 +1100

    Internal javadocs

--- nebula-inject-test/src/main/java/dev/nebulamc/inject/test/MockFactory.java
+++ nebula-inject-test/src/main/java/dev/nebulamc/inject/test/MockFactory.java
@@ -10,5 +10,14 @@ import org.jspecify.nullness.NullMarked;
 @NullMarked
 interface MockFactory {
 
+    /**
+     * Creates a mock object with the specified options.
+     *
+     * @param type the type of the mock object.
+     * @param options the options for the mock object.
+     * @return the mock object.
+     * @param <T> the type of the mock object.
+     * @throws RuntimeException if an error occurs while creating the mock object.
+     */
     <T> T createMock(final Class<T> type, final Mock options);
 }
