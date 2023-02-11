e47d233c59e90f03eb112e21896e28f332830699
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-02-11 18:16:01 +1100

    Add maven publishing

--- nebula-inject-test/build.gradle.kts
+++ nebula-inject-test/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    `java-library`
+    id("nebula-inject.library-conventions")
 }
 
 repositories {
@@ -14,8 +14,6 @@ dependencies {
     api("org.mockito:mockito-core:4.11.0")
     api(project(":nebula-inject"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("org.junit.platform:junit-platform-launcher:1.9.2")
 }
 
--- nebula-inject/build.gradle.kts
+++ nebula-inject/build.gradle.kts
@@ -1,30 +1,10 @@
 plugins {
-    `java-library`
-}
-
-repositories {
-    mavenCentral()
+    id("nebula-inject.library-conventions")
 }
 
 dependencies {
     compileOnly("org.jspecify:jspecify:0.2.0")
     // TODO: Update to 0.3.0 when more tools add support
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("org.mockito:mockito-core:4.11.0")
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-    withJavadocJar()
-    withSourcesJar()
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-    }
 }
